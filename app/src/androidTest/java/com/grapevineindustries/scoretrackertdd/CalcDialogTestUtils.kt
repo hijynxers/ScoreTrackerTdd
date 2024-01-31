af0d8a5b996bd54ecae49d0b10d1597b9325b05d
Nathan Welch <nathan.a.welch@outlook.com>
2024-01-30 21:05:21 -0600

    calc dialog - buttons add to score and factors

---
+++ app/src/androidTest/java/com/grapevineindustries/scoretrackertdd/CalcDialogTestUtils.kt
@@ -0,0 +1,55 @@
+package com.grapevineindustries.scoretrackertdd
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertTextEquals
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.grapevineindustries.scoretrackertdd.ui.composables.CalcDialogTestTags
+
+object CalcDialogTestUtils {
+    private lateinit var composeTestRule: ComposeTestRule
+
+    fun setup(rule: ComposeTestRule) {
+        composeTestRule = rule
+    }
+
+    private val buttonList = listOf("3","4","5","6","7","8","9","10","J","Q","K","JOKE")
+
+    fun assertShowing() {
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.CALC_DIALOG)
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.OK)
+            .assertIsDisplayed()
+            .assertTextEquals("OK")
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.SUM)
+            .assertIsDisplayed()
+            .assertTextEquals("0")
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.FACTORS)
+            .assertIsNotDisplayed()
+
+        buttonList.forEachIndexed { index, num ->
+            composeTestRule.onNodeWithTag(CalcDialogTestTags.BUTTON + buttonList[index])
+                .assertIsDisplayed()
+                .assertTextEquals(buttonList[index])
+        }
+    }
+
+    fun assertSum(sum: String) {
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.SUM)
+            .assertTextEquals(sum)
+    }
+
+    fun assertFactors(factors: String) {
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.FACTORS)
+            .assertTextEquals(factors)
+    }
+
+    fun clickButton(buttonNum: String) {
+        composeTestRule.onNodeWithTag(CalcDialogTestTags.BUTTON + buttonNum)
+            .performClick()
+    }
+
+
+}
--- app/src/main/java/com/grapevineindustries/scoretrackertdd/ui/composables/CalcDialog.kt
+++ app/src/main/java/com/grapevineindustries/scoretrackertdd/ui/composables/CalcDialog.kt
@@ -1,15 +1,25 @@
 package com.grapevineindustries.scoretrackertdd.ui.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -26,10 +36,15 @@ fun CalcDialogPreview() {
     }
 }
 
+private val numbers = listOf(3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 50)
+
 @Composable
 fun CalcDialog(
     closeCalcDialog: () -> Unit
 ) {
+    val sum = remember { mutableIntStateOf(0) }
+    val factors = remember { mutableStateOf("") }
+
     Dialog(onDismissRequest = { /*TODO*/ }) {
         Card(
             shape = RoundedCornerShape(10.dp),
@@ -39,10 +54,54 @@ fun CalcDialog(
             ),
             content = {
                 Column(
-                    modifier = Modifier.testTag(CalcDialogTestTags.CALC_DIALOG)
+                    modifier = Modifier
+                        .testTag(CalcDialogTestTags.CALC_DIALOG)
+                        .padding(all = 8.dp), horizontalAlignment = AbsoluteAlignment.Right
                 ) {
+
+                    Text(
+                        modifier = Modifier.testTag(CalcDialogTestTags.FACTORS),
+                        text = factors.value,
+                        textAlign = TextAlign.Right
+                    )
+                    Text(
+                        modifier = Modifier.testTag(CalcDialogTestTags.SUM),
+                        fontWeight = FontWeight.Bold,
+                        text = sum.intValue.toString()
+                    )
+
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(3)
+                    ) {
+                        items(numbers) { element ->
+                            val convertedElement = convertWildCard(element)
+                            Button(
+                                modifier = Modifier
+                                    .testTag(CalcDialogTestTags.BUTTON + convertedElement)
+                                    .padding(4.dp),
+                                onClick = {
+                                    sum.intValue = sum.intValue + element
+                                    factors.value = factors.value + "  $convertedElement"
+                                }
+                            ) {
+                                Text(
+                                    text = convertedElement,
+                                    fontWeight = FontWeight.Bold,
+                                    textAlign = TextAlign.Center,  // horizontal center of the text
+                                )
+                            }
+                        }
+                    }
+
                     Button(
-                        modifier = Modifier.testTag(CalcDialogTestTags.OK),
+                        modifier = Modifier
+                            .testTag(CalcDialogTestTags.OK)
+                            .padding(
+                                start = 4.dp,
+                                end = 4.dp,
+                                top = 12.dp
+                            )
+                            .fillMaxWidth(),
                         onClick = closeCalcDialog
                     ) {
                         Text(
@@ -55,7 +114,22 @@ fun CalcDialog(
     }
 }
 
+@Composable
+fun convertWildCard(card: Int): String {
+    var ret = card.toString()
+    when (card) {
+        11 -> ret = "J"
+        12 -> ret = "Q"
+        13 -> ret = "K"
+        50 -> ret = "JOKE"
+    }
+    return ret
+}
+
 object CalcDialogTestTags {
     const val CALC_DIALOG = "CALC_DIALOG"
     const val OK = "CALC_DIALOG_OK"
+    const val BUTTON = "CALC_DIALOG_BUTTON_"
+    const val SUM = "CALC_DIALOG_SUM"
+    const val FACTORS = "CALC_DIALOG_FACTORS"
 }
